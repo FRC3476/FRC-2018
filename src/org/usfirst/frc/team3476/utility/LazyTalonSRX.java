873eb3d53906a346fe53c152ba21bfb2a781ae0e
Vineet Palepu <vineetmpalepu@gmail.com>
2018-02-11 16:16:55 -0800

    Made LazyTalon wrapper and replaced all instances of TalonSRX

---
+++ src/org/usfirst/frc/team3476/utility/LazyTalonSRX.java
@@ -0,0 +1,19 @@
+package org.usfirst.frc.team3476.utility;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+public class LazyTalonSRX extends TalonSRX {
+
+	public LazyTalonSRX(int deviceNumber) {
+		super(deviceNumber);
+	}
+	
+	@Override
+	public void set(ControlMode controlMode, double outputValue)
+	{
+		if (controlMode != super.getControlMode() || outputValue != super.getClosedLoopTarget(0))
+			super.set(controlMode, outputValue);
+	}
+
+}

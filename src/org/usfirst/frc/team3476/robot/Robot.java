619bc61c50b651ae4e7024960abe1b3c8b615ca3
Vineet Palepu <vineetmpalepu@gmail.com>
2018-03-13 21:24:58 -0700

    E

--- src/org/usfirst/frc/team3476/robot/Robot.java
+++ src/org/usfirst/frc/team3476/robot/Robot.java
@@ -217,7 +217,7 @@ public class Robot extends IterativeRobot {
 		
 		drive.cheesyDrive(-xbox.getRawAxis(1), -xbox.getRawAxis(4), xbox.getRawAxis(2) > .3);
 		//drive.arcadeDrive(-xbox.getRawAxis(1), -xbox.getRawAxis(4));
-		//System.out.println("Angle: " + elevarm.getArmAngle()+ " Setpoint: " + elevarm.getTargetArmAngle());
+		System.out.println("Angle: " + elevarm.getArmAngle()+ " Setpoint: " + elevarm.getTargetArmAngle());
 		//System.out.println("Height: " + elevarm.getElevatorHeight() + " Setpoint: " + elevarm.getTargetElevatorHeight());
 		
 		if (joystick.getRisingEdge(2))
@@ -278,11 +278,11 @@ public class Robot extends IterativeRobot {
 		double nudge = joystick.getRawAxis(1);
 		if (nudge > Constants.JoystickDeadzone)
 		{
-			elevarm.setElevatorHeight(elevarm.getElevatorHeight() - (nudge - Constants.JoystickDeadzone) * 4);
+			elevarm.setElevatorHeight(elevarm.getElevatorHeight() - (nudge - Constants.JoystickDeadzone) * 5);
 		}
 		else if (nudge < -Constants.JoystickDeadzone)
 		{
-			elevarm.setElevatorHeight(elevarm.getElevatorHeight() - (nudge + Constants.JoystickDeadzone) * 4);
+			elevarm.setElevatorHeight(elevarm.getElevatorHeight() - (nudge + Constants.JoystickDeadzone) * 5);
 		}
 		
 		
@@ -321,11 +321,13 @@ public class Robot extends IterativeRobot {
 		
 		if (buttonBox.getPOV() == 0)
 		{
-			elevarm.setOverallPosition(elevarm.getDistance() + 1, elevarm.getHeight());
+			//elevarm.setOverallPosition(elevarm.getDistance() + 1, elevarm.getHeight());
+			elevarm.setArmAngle(elevarm.getArmAngle() - 3);
 		}
 		else if (buttonBox.getPOV() == 180)
 		{
-			elevarm.setOverallPosition(elevarm.getDistance() - 1, elevarm.getHeight());
+			//elevarm.setOverallPosition(elevarm.getDistance() - 1, elevarm.getHeight());
+			elevarm.setArmAngle(elevarm.getArmAngle() + 3);
 		}
 		
 		//if (buttonBox.getRisingEdge(button))
--- src/org/usfirst/frc/team3476/subsystem/Elevarm.java
+++ src/org/usfirst/frc/team3476/subsystem/Elevarm.java
@@ -30,6 +30,8 @@ public class Elevarm extends Threaded {
 	private ElevatorState currentElevatorState = ElevatorState.MANUAL;
 	private RateLimiter elevatorLimiter;
 	private volatile double elevatorSetpoint;
+	
+	private boolean elevatorIntakePositionSet = false;
 
 	private Elevarm() {
 		elevatorLimiter = new RateLimiter(1000, 120);
@@ -236,13 +238,14 @@ public class Elevarm extends Threaded {
 			elevator.setHeight(elevatorLimiter.update(Constants.ElevatorDownHeight));
 			if (elevator.getHeight() < 20)
 			{
-				arm.setAngle(-10);
+				arm.setAngle(-18);
 			}
-			if (Math.abs(arm.getAngle() + 10) < 4)
+			/*if (Math.abs(arm.getAngle() + 10) < 5)
 			{
 				arm.setPercentOutput(0);
+				elevatorIntakePositionSet = true;
 			}
-			
+			*/
 			break;
 		case MANUAL:
 			break;
